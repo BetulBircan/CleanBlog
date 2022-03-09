f7cf266144d77b1ea3c7e04451288867019ce887
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-03-09 20:28:59 +0300

    Request | app.get ile request yollandı ve blog ile bilgiler response olarak dönüldü.

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+//Express i sayfaya ekleme
 const express = require('express');
 
+//app değişkenşne express fonksiyonunu atama
 const app =express();
 
+//Get metodu iel request yollama ve res.send ile de blog ile bilgileri response olarak dönme
+app.get('/', (req, res) => {
+    const blog = {
+        id: 1,
+        title: "Blog Title",
+        description: "Blog Description"
+    }
+    res.send(blog)
+})
+
+//Port numarası tanımlama ve o port üzerinden sunucuyu başlatma
 const port = 3300
 app.listen(port, () => {
     console.log(`Sunucu ${port} numaralı porrta başlatıldı`)
