3c164c25ad94224ac6d4cd4ff20d2942d3f34650
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-03-10 18:32:23 +0300

    MiddleWare | Projeye static dosyalar public clasörğğne eklendi ve index dosyaları templates dosyalarına atıldı daha sonra da index.html dosyası response olarak dönüldü

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 //Express i sayfaya ekleme
 const express = require('express');
+//statik dosyaları response olarak döndürmek için path modülünü app.js sayfasına dahil etme
+const path = require('path'); 
 
 //app değişkenşne express fonksiyonunu atama
 const app =express();
 
-//Get metodu iel request yollama ve res.send ile de blog ile bilgileri response olarak dönme
+//Middleware
+app.use(express.static('public'))
+
+//Get metodu ile request yollama ve res.send ile de blog ile bilgileri response olarak dönme
 app.get('/', (req, res) => {
-    const blog = {
-        id: 1,
-        title: "Blog Title",
-        description: "Blog Description"
-    }
-    res.send(blog)
+    //template klasöründeki index.html dosyasını resolve olarak döndürme
+    res.sendFile(path.resolve(__dirname, 'template/index.html'))
 })
 
 //Port numarası tanımlama ve o port üzerinden sunucuyu başlatma
