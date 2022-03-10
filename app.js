4fe9423aa57a69409958fc83cfe6fadf62525635
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-03-10 22:53:23 +0300

    app.js de ejs öodülü kullanılacağı belirtildi.Views klasörü oluşturuldu html dosyaları ejs ye çevrildi.partial klasör yapısı oluşturuldu

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const path = require('path');
 //app değişkenşne express fonksiyonunu atama
 const app =express();
 
+/*EJS modülü template dosyaları görebilmek için varsayılan olarak views
+ klasörünün içerisindeki .ejs uzantılı dosyalara bakar. Bu ne denle temp dosyamızın 
+ ismini views olarak değiştiriyoruz. Videws klasörü içerisindeki tüm .html uzantılarını 
+ .ejs olarak değiştiriyoruz.  */
+
+ //Template Engine
+ app.set('view engine', 'ejs')
+
 //Middleware
 app.use(express.static('public'))
 
