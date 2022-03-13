eb27be9c4e01eefc7ffa00bd296b93234ef1fa09
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-03-13 22:44:15 +0300

    Add New Post sayfasından gönderilecek veriler req.body ile yakalalandı, gerekli middleware fonksiyonları kullanıldı.

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 //Express i sayfaya ekleme
 const express = require('express');
+//mongoose u kullanmak için app.js dosyasına ekliyoruz.
+const mongoose = require('mongoose');
 //ejs tempalte şablonunu kullanmak için ejs modülünü app.js sayfasına dahil etme
 const ejs = require('ejs');
-//app değişkenşne express fonksiyonunu atama
+//app değişkenine express fonksiyonunu atama
 const app = express();
+
+//mongoose ile veritabanına bağlanma
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 /*EJS modülü template dosyaları görebilmek için varsayılan olarak views
  klasörünün içerisindeki .ejs uzantılı dosyalara bakar. Bu ne denle temp dosyamızın 
@@ -14,7 +22,9 @@ const app = express();
 app.set('view engine', 'ejs');
 
 //Middleware
-app.use(express.static('public'));
+app.use(express.static('public')); //index.html,css gibi statik dosyaları ekleme
+app.use(express.urlencoded({extended:true})) //url deki datayı okumamızı sağlar
+app.use(express.json()) //url deki datayı json formatına dönüştürmemizi sağlar.
 
 //Routers
 app.get('/', (req, res) => {
@@ -23,19 +33,20 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-    //Uygulamamızdaki .get metodunu düzenlersek, bu şekilde '/about' isteğine karşılık about.ejs dosyasını render ederiz.
-    res.render('about');
-  });
+  //Uygulamamızdaki .get metodunu düzenlersek, bu şekilde '/about' isteğine karşılık about.ejs dosyasını render ederiz.
+  res.render('about');
+});
 
-  app.get('/addpost', (req, res) => {
-    //Uygulamamızdaki .get metodunu düzenlersek, bu şekilde '/addpost' isteğine karşılık add_post.ejs dosyasını render ederiz.
-    res.render('add_post');
-  });
+app.get('/addpost', (req, res) => {
+  //Uygulamamızdaki .get metodunu düzenlersek, bu şekilde '/addpost' isteğine karşılık add_post.ejs dosyasını render ederiz.
+  res.render('add_post');
+});
 
-  app.get('/post', (req, res) => {
-    //Uygulamamızdaki .get metodunu düzenlersek, bu şekilde '/post' isteğine karşılık post.ejs dosyasını render ederiz.
-    res.render('post');
-  });
+app.post('/post',  (req, res) => {
+  //Uygulamamızdaki .get metodunu düzenlersek, bu şekilde '/post' isteğine karşılık post.ejs dosyasını render ederiz.
+   console.log(req.body) 
+  res.redirect('/');
+});
 
 //Port numarası tanımlama ve o port üzerinden sunucuyu başlatma
 const port = 3300;
